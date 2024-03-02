bc97a601fe9af62a17ac886ec284ef67188824c9
Iliyas128 <Weking128@gmail.com>
2024-03-02 09:25:34 +0600

    Second commi

---
+++ NoIdea/Button.py
@@ -0,0 +1,41 @@
+import pygame
+
+
+# Button.py or within your main game file
+class Button:
+    def __init__(self, x, y, width, height, text='', color=(73, 73, 73), highlight_color=(189, 189, 189),
+                 function=None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.color = color
+        self.highlight_color = highlight_color
+        self.function = function
+
+    def draw(self, win, outline=None):
+        if outline:
+            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
+
+        pygame.draw.rect(win, self.color if not self.is_over(pygame.mouse.get_pos()) else self.highlight_color,
+                         (self.x, self.y, self.width, self.height), 0)
+
+        if self.text != '':
+            font = pygame.font.SysFont('comicsans', 60)
+            text = font.render(self.text, 1, (0, 0, 0))
+            win.blit(text, (
+            self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
+
+    def is_over(self, pos):
+        # Pos is the mouse position or a tuple of (x,y) coordinates
+        if self.x < pos[0] < self.x + self.width and self.y < pos[1] < self.y + self.height:
+            return True
+        return False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.is_over(pygame.mouse.get_pos()):
+                if self.function:
+                    self.function()
+
