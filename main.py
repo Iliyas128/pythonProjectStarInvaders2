52400cf214d074283827261bad67959d53db0989
Iliyas128 <Weking128@gmail.com>
2024-03-03 00:29:20 +0600

    Without errors

--- main.py
+++ main.py
@@ -2,44 +2,10 @@ import pygame
 import random
 import math
 from pygame import mixer
-
+from Noidea.Button import Button
 # Initialize Pygame
 pygame.init()
 
-
-class Button:
-    def __init__(self, x, y, width, height, text='', color=(73, 73, 73), highlight_color=(189, 189, 189),
-                 function=None):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.text = text
-        self.color = color
-        self.highlight_color = highlight_color
-        self.function = function
-
-    def draw(self, win, outline=None):
-        if outline:
-            pygame.draw.rect(win, outline, (self.x - 2, self.y - 2, self.width + 4, self.height + 4), 0)
-        pygame.draw.rect(win, self.color if not self.is_over(pygame.mouse.get_pos()) else self.highlight_color,
-                         (self.x, self.y, self.width, self.height), 0)
-        if self.text != '':
-            font = pygame.font.SysFont('comicsans', 60)
-            text = font.render(self.text, 1, (0, 0, 0))
-            win.blit(text, (
-                self.x + (self.width / 2 - text.get_width() / 2), self.y + (self.height / 2 - text.get_height() / 2)))
-
-    def is_over(self, pos):
-        if self.x < pos[0] < self.x + self.width and self.y < pos[1] < self.y + self.height:
-            return True
-        return False
-
-    def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.is_over(pygame.mouse.get_pos()):
-                if self.function:
-                    self.function()
 
 
 # Create the screen
@@ -62,7 +28,10 @@ icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
 
 # Player
-player_img = pygame.image.load('player.png').convert_alpha()
+try:
+    player_img = pygame.image.load('player.png').convert_alpha()
+except Exception as e:
+    print(f"Error loading player image: {e}")
 player_x = SCREEN_WIDTH / 2 - 32
 player_y = SCREEN_HEIGHT - 100
 player_x_change = 0
@@ -358,7 +327,7 @@ def main():
                     elif quitt_button.is_over(pygame.mouse.get_pos()):
                         quitt_button.handle_event(event)
 
-        # Make sure to update the display after drawing everything
+
         pygame.display.update()
 
 
